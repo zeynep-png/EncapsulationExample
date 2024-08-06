f67086ddcd760ee576368a29bb501474da17aab9
Zeynep Tuana Çakır <2320711004@ogrenci.ibu.edu.tr>
2024-08-06 10:29:52 +0300

    Add project files.

---
+++ EncapsulationExample/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExample
+{
+    public class Car
+    {
+        //proporties PascalCasing
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        private int numberOfDoors;
+
+        public Car(string brand, string model, string color, int numberOfDoors)
+        {
+            Brand = brand; //Fields camelCasing
+            Model = model;
+            Color = color;
+            NumberOfDoors = numberOfDoors; //encapsulated property
+
+        }
+
+
+        public int NumberOfDoors { get { return numberOfDoors; } 
+            set {
+                if (value == 2 || value == 4)
+                    numberOfDoors = value;
+                else {
+                    Console.WriteLine("Geçersiz bir değer atanız.");
+                    numberOfDoors = -1;
+
+                }
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Brand: {Brand}\nModel: {Model}\nColor: {Color}\nNumber of Doors: {NumberOfDoors}");
+        }
+    }
+}
---
+++ EncapsulationExample/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using EncapsulationExample;
+
+Car car1 = new Car("Honda", "Civic" , "White", 4);
+car1.Display();
+
+Console.WriteLine("\n\n------------------------------------\n\n");
+
+Car car2 = new Car("Toyota", "Corolla", "Black", 3); // This will throw an error message
+car2.Display();
